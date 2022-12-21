b12bcee078de9f33a5cef13526b2f75418fa5137
Evgenii <markelovea1990@yandex.ru>
2022-12-22 00:31:03 +0300

    End the day

--- app/src/main/java/com/example/notebook/domain/InMemoryCitiesReposirtory.java
+++ app/src/main/java/com/example/notebook/domain/InMemoryCitiesReposirtory.java
@@ -2,6 +2,10 @@ package com.example.notebook.domain;
 
 import android.content.Context;
 
+import com.example.notebook.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InMemoryCitiesReposirtory implements CitiesRepository {
@@ -9,6 +13,9 @@ public class InMemoryCitiesReposirtory implements CitiesRepository {
     private static CitiesRepository INSTANCE;
 
     public static CitiesRepository getINSTANCE(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE= new InMemoryCitiesReposirtory(context);
+        }
         return INSTANCE;
     }
 
@@ -21,7 +28,12 @@ public class InMemoryCitiesReposirtory implements CitiesRepository {
 
     @Override
     public List<City> getAll() {
-        return null;
+        ArrayList<City> result = new ArrayList<>();
+        result.add(new City(context.getString(R.string.kazan), R.drawable.kazan_gerb));
+        result.add(new City(context.getString(R.string.ekb), R.drawable.ekaterinburg_gerb));
+        result.add(new City(context.getString(R.string.nn), R.drawable.nizhny_novgorod_gerb));
+        result.add(new City(context.getString(R.string.novosib), R.drawable.novosibirsk_gerb));
+        return result;
     }
 
     @Override
---
+++ app/src/main/java/com/example/notebook/ui/CitiesListFragment.java
@@ -0,0 +1,7 @@
+package com.example.notebook.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class CitiesListFragment extends Fragment {
+
+}
