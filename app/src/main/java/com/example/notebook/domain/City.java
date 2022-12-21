3f9471ac72a0139af37eaa05de851e95619ac958
Evgenii <markelovea1990@yandex.ru>
2022-12-21 23:17:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notebook/domain/City.java
@@ -0,0 +1,12 @@
+package com.example.notebook.domain;
+
+import androidx.annotation.DrawableRes;
+
+public class City {
+    private String name;
+    private @DrawableRes
+    int icon;
+    
+
+
+}
